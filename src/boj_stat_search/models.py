895da18a5b8c19ac6c34ed5ec811c7d94c3bd665
saviour <savioursho@gmail.com>
2026-02-21 15:17:41 +0900

    update

--- src/boj_stat_search/models.py
+++ src/boj_stat_search/models.py
@@ -1,7 +1,39 @@
 from dataclasses import dataclass
 
-@dataclass
+@dataclass(frozen=True)
 class DbInfo:
     name: str
     desc: str
     category: str
+
+@dataclass(frozen=True)
+class MetadataEntry:
+    series_code: str
+    name_of_time_series_j: str
+    name_of_time_series: str
+    unit_j: str
+    unit: str
+    frequency: str
+    category_j: str
+    category: str
+    layer1: int
+    layer2: int
+    layer3: int
+    layer4: int
+    layer5: int
+    start_of_the_time_series: str
+    end_of_the_time_series: str
+    last_update: str
+    notes_j: str
+    notes: str
+
+@dataclass(frozen=True)
+class MetadataResponse:
+    status: int
+    message_id: str
+    message: str
+    date: str
+    db: str
+    result_set: tuple[MetadataEntry, ...]
+
+
